ca0f37deffc8f3f43031954c1701bea3ab44104c
S3rdna <aaguilar8079@gmail.com>
2023-01-14 12:32:18 -0800

    Revert "pre threading"
    This reverts commit 0874c8af6e172b5f37dc07705737f4bd260eaf39.

--- minecraft_chan_public_code.py
+++ minecraft_chan_public_code.py
@@ -1,37 +1,20 @@
 import discord
 import os 
 from google.oauth2 import service_account
+import google.auth.transport.requests
 from google.auth.transport.requests import AuthorizedSession
 from mcstatus import JavaServer
-from idle_check import *
-import threading 
 
 intents = discord.Intents.default()
 intents.message_content = True 
 client = discord.Client(intents=intents)
-start_url = "https://us-west2-minecraft-server-360820.cloudfunctions.net/benzene-server-start"
-stop_url = "https://us-west2-minecraft-server-360820.cloudfunctions.net/benzene-server-stop"
-service_account_creds = "./MINECRAFT_CHAN_CREDENTIALS.json"
+start_url = os.environ['MINECRAFT_CHAN_START_URL']
+stop_url = os.environ['MINECRAFT_CHAN_STOP_URL']
+service_account_creds = os.environ['MINECRAFT_CHAN_CREDS']
 stop_session_creds = service_account.IDTokenCredentials.from_service_account_file(service_account_creds,target_audience=stop_url)
 start_session_creds = service_account.IDTokenCredentials.from_service_account_file(service_account_creds,target_audience=start_url)
 MChan_Token = os.environ['MINECRAFT_CHAN_TOKEN']
 server_ip = os.environ['MC_SERVER']
-
-
-# create a thread to check if the server is up
-# if it is, then send a message to the discord channel
-# if it is not, then send a message to the discord channel
-
-def thread_idle_check():
-    conn = createDB()
-    while True:
-        timeStamp = datetime.datetime.now()
-        isUp = checkStatus()
-        insertDB(conn, "up" if isUp == 0 else "down" , timeStamp)
-        rows = findLastUp(conn)
-        findDeltaTime(rows)
-        time.sleep(30)
-        
 
 @client.event
 async def on_message(message): 
@@ -45,15 +28,13 @@ async def on_message(message):
             response_code = response.status_code
             if response_code == 200:
                 await message.channel.send('#Starting the Benzene Server!  ٩(＾◡＾)۶')
-                await message.channel.send(file=discord.File('./media/creeper_chan_smile.jpg'))
+                await message.channel.send(file=discord.File('../Additional Files/creeper_chan_smile.jpg'))
             else:
                 await message.channel.send('#Unable to start the server! ( •_•)')
-                await message.channel.send(file=discord.File('./media/404_chan.png'))
-        except FileNotFoundError:
-            await message.channel.send('IMAGE FILE NOT FOUND')
+                await message.channel.send(file=discord.File('../Additional Files/404_chan.png'))
         except:
             await message.channel.send('#Unable to start the server! ( •_•)')
-            await message.channel.send(file=discord.File('./media/404_chan.png'))
+            await message.channel.send(file=discord.File('../Additional Files/404_chan.png'))
             
         
 
@@ -65,13 +46,13 @@ async def on_message(message):
             response_code = response.status_code
             if response_code == 200:
                 await message.channel.send('#Shutting Down the Benzene Server! (>﹏<)')
-                await message.channel.send(file=discord.File('./media/creeper_chan_goodbye.jpg')) 
+                await message.channel.send(file=discord.File('../Additional Files/creeper_chan_goodbye.jpg')) 
             else:
                 await message.channel.send('#Unable to stop the server! ( •_•)')
-                await message.channel.send(file=discord.File('./media/404_chan.png'))
+                await message.channel.send(file=discord.File('../Additional Files/404_chan.png'))
         except:
             await message.channel.send('#Unable to stop the server! ( •_•)')
-            await message.channel.send(file=discord.File('./media/404_chan.png'))
+            await message.channel.send(file=discord.File('../Additional Files/404_chan.png'))
          
 
     if message.content.startswith('$status'):
@@ -84,10 +65,10 @@ async def on_message(message):
             if response == 0:
                 await message.channel.send('#The Benzene Server is up!  ( ͡° ͜ʖ ͡°)')
                 await message.channel.send('#There are currently {0} {1} online!'.format(server.status().players.online, 'player' if server.status().players.online == 1 else 'players' ))
-                await message.channel.send(file=discord.File('./media/creeper_chan_smile.jpg'))
+                await message.channel.send(file=discord.File('../Additional Files/creeper_chan_smile.jpg'))
             else:
                 await message.channel.send('#The Benzene Server is down!  (ಥ _ ಥ)')
-                await message.channel.send(file=discord.File('./media/404_chan.png'))
+                await message.channel.send(file=discord.File('../Additional Files/404_chan.png'))
         except TimeoutError:
             pass
         
